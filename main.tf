81f38a011f08c1cd9ef756054df47a9485044769
b1kjsh <joshuaman32@gmail.com>
2023-08-20 22:59:32 -0400

    adding a comment

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ data "aws_ami" "app_ami" {
 
   owners = ["979382823631"] # Bitnami
 }
-
+# this is a comment
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
